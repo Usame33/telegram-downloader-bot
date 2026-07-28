600b6faa79dd4981ab61f685aca69eaf6859c997
Usame33 <use9811@gmail.com>
2026-07-28 19:46:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,18 @@
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running 24/7!"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+threading.Thread(target=run_flask).start()
 import os
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
