913a86b63ef00cb0c2973d7831d4468a17f86d55
Usame33 <use9811@gmail.com>
2026-07-28 20:53:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,8 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import yt_dlp
 
-TOKEN = "8629100412:AAFbCinwIOHvhSwvReg2l67-K9dqUgHpyjM"
+TOKEN = os.environ.get("TOKEN")
+
 CHANNEL = "@wanasatt"
 CHANNEL_LINK = "https://t.me/wanasatt"
 
