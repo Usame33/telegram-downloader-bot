9837fb64c0437968f57d5e24481eb93cc88ef823
Usame33 <use9811@gmail.com>
2026-07-30 22:25:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# سيرفر وهمي لإرضاء Render
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is alive!")
+
+def run_dummy_server():
+    port = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+# تشغيل السيرفر في خلفية النظام
+threading.Thread(target=run_dummy_server, daemon=True).start()
 import os
 import sys
 import time
